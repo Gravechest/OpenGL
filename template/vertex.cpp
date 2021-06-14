b3b9ff9e5dae1ebd9e371c509d9ba391fe7dc3e8
Gravechest <75589056+Gravechest@users.noreply.github.com>
2021-06-14 17:47:16 +0200

    Create vertex.cpp

---
+++ template/vertex.cpp
@@ -0,0 +1,6 @@
+const char* vertexshaderSource = "#version 330 core\n"
+"layout (location = 0) in vec2 verticles;"
+"uniform float playerpos;"
+"void main(){"
+"gl_Position = vec4(vec2(verticles[0] + playerpos,verticles[1]),1.0,1.0);"
+"}";
